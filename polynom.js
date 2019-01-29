52bf1c7ada1936693a7d9df34747f5ed4a47f496
Lukáš Chábek <lukaschabek@gmail.com>
2019-01-29 01:37:38 +0100

    Initialize Repository
    Project is already done, just need a little refactoring.

---
+++ polynom.js
@@ -0,0 +1,61 @@
+class Polynom
+{
+	constructor(constant, exponent)
+	{
+		this.constant = constant;
+		this.exponent = exponent;
+
+	}
+
+	get()
+	{
+		if (this.exponent == 0)
+			return this.constant;
+		return this.constant + "x^" + this.exponent;
+	}
+
+	add(constant)
+	{
+		return new Polynom(this.constant - constant, this.exponent);
+	}
+
+	sub(constant)
+	{
+		return this.add(-constant);
+	}
+
+	sub_poly(p)
+	{
+		return new Polynom(this.constant - p.constant, this.exponent);
+	}
+
+	mul_poly(p)
+	{
+		return new Polynom(this.constant * p.constant, this.exponent + p.exponent);
+	}
+
+	div_poly(p)
+	{
+		return new Polynom(this.constant / p.constant, this.exponent - p.exponent);
+	}
+
+	compare_poly(p)
+	{
+		if (this.exponent > p.exponent)
+			return 1;
+		if (this.exponent == p.exponent)
+			return 0;
+		if (this.exponent < p.exponent)
+			return -1;
+	}
+
+	clone()
+	{
+		return new Polynom(this.constant, this.exponent);
+	}
+
+	neg()
+	{
+		return new Polynom(-this.constant, this.exponent);
+	}
+}
---
+++ polynomial.js
@@ -0,0 +1,85 @@
+let P = (a, x) => {
+	return new Polynom(a, x)
+};
+
+function print_polygon(pol) {
+	output = '';
+	for (const p of pol) {
+		if (p.constant < 0)
+			output += " - " + p.neg().get();
+		else
+			output += " + " + p.get();
+	}
+	return output.slice(1);
+}
+
+function clone_poly_array(arr)
+{
+	output = [];
+	for (const p of arr) {
+		output.push(p.clone())
+	}
+
+	return output;
+}
+
+function index_of_poly_exponent(arr, exp)
+{
+	for (let i = 0; i < arr.length; i++) {
+		const element = arr[i];
+		if (element.exponent == exp)
+			return i;
+	}
+	return -1;
+}
+
+function divide(a, b) {
+	let result = [];
+	let reminder = [];
+	let divider = clone_poly_array(b);
+	do {
+		if (a[0].compare_poly(b[0]) >= 0) {
+			reminder = [];
+			result.push(a[0].div_poly(b[0]));
+
+			for (let i = 0; i < divider.length; i++) {
+				divider[i] = divider[i].mul_poly(result[result.length - 1]);
+			}
+
+			let len = a[0].exponent + 1
+			for (let i = 0; i < len; i++) {
+				const aidx = index_of_poly_exponent(a, i);
+				const didx = index_of_poly_exponent(divider, i);
+
+				if (didx == -1)
+				{
+					reminder.push(a[aidx])
+					continue;
+				}
+
+				if (aidx == -1)
+				{
+					reminder.push(divider[didx].neg())
+					continue;
+				}
+
+				let sub_result = a[aidx].sub_poly(divider[didx]);
+				if (sub_result.constant != 0)
+					reminder.push(sub_result)
+			}
+
+			a = clone_poly_array(reminder).reverse();
+			divider = clone_poly_array(b);
+		} else
+			break;
+	} while (true);
+	reminder.reverse();
+	return {reminder, result};
+}
+
+let a = [P(4, 4), P(3, 3), P(2, 2), P(1, 1), P(1, 0)];
+let b = [P(2, 2), P(3, 1), P(1, 0)];
+let r = divide(a, b);
+$("#inputCalc").val(print_polygon(a) + " / " + print_polygon(b))
+$("#outputCalc").text(print_polygon(r.result) + " | " + print_polygon(r.reminder));
+console.table({Result: print_polygon(r.result), Reminder: print_polygon(r.reminder)});
